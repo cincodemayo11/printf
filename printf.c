c30e018e8bb868d63b5795ff51f9cde39a7312f8
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-03-28 09:12:08 +0000

    A function that produces output according to a format

--- printf.c
+++ printf.c
@@ -1,37 +1,71 @@
-#include <stdarg.h>
-#include <stdio.h>
+#include "main.h"
+
+/**
+  * _printf - prints output
+  * @format: string input
+  *
+  * Return: Number of characters printed
+  */
 
 int _printf(const char *format, ...)
 {
-va_list list;
-va_start(list, format);
+	va_list list;
+	int length = 0;
 
-
-while (*format != '\0')
-{
-	if (*format == '%')
+	va_start(list, format);
+	if (format != NULL)
 	{
-		format++;
-		if (*format == 'c')
+		while (*format != '\0')
 		{
-			printf("%c", va_arg(list, int));
+			if (*format == '%')
+			{
+				format++;
+				if (*format == 'c')
+				{
+					printf("%c", va_arg(list, int));
+					length++;
+				}
+				else if (*format == 's')
+				{
+					print_string(va_arg(list, char *), length);
+				}
+				else if (*format == '%')
+				{
+					printf("%%");
+					length++;
+				}
+				format++;
+			}
+			else
+			{
+				printf("%c", *format);
+				length++;
+				format++;
+			}
 		}
-		else if (*format == 's')
-		{
-			printf("%s", va_arg(list, char *));
-		}
-		else if (*format == '%')
-		{
-			printf("%%");
-		}
-		format++;
 	}
 	else
 	{
-		printf("%c", *format);
-		format++;
+		length += length;
 	}
-
+	return (length);
 }
-return (0);
+
+/**
+  * print_string - print a string
+  * @string: the string to be printed
+  * @length: variable to hold the length of the string
+  *
+  * Return: 0
+  */
+int print_string(char *string, int length)
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
+	{
+		putchar(string[i]);
+		length++;
+	}
+	return (0);
 }
