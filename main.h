59fac0e96210279dcae99b54be547e6f4b2fb23e
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-03-28 09:14:27 +0000

    Headers file

--- main.h
+++ main.h
@@ -1,1 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <strings.h>
+#include <stddef.h>
+#include <stdarg.h>
 int _printf(const char *format, ...);
+int print_string(char *string, int length);
+#endif
